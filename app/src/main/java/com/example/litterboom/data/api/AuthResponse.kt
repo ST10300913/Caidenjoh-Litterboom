eead387ec6b90a6d4466d9721370fcd300714bdb
st10344544 <camcowdrey@gmail.com>
2025-10-30 12:54:35 +0200

    Implementation of api part 1

--- app/src/main/java/com/example/litterboom/data/api/AuthResponse.kt
+++ app/src/main/java/com/example/litterboom/data/api/AuthResponse.kt
@@ -1,3 +1,3 @@
 package com.example.litterboom.data.api
 
-data class AuthResponse(val token: String, val role: String)
+data class AuthResponse(val token: String, val role: String, val userId: Int)
